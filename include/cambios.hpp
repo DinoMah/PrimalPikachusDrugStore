ec87a94b29c68e87d93012551c67ab74616877ab
DinoMah <leithonpeas@hotmail.com>
2020-10-11 00:05:15 -0500

    Cambios iniciado

--- include/cambios.hpp
+++ include/cambios.hpp
@@ -2,12 +2,28 @@
 #define CAMBIOS_H
 
 #include "../src/objeto.cpp"
+#include <string>
 
-void cambiarnom( objeto & );
-void cambiardesc( objeto & );
-void cambiarexis( objeto & );
-void cambiarfecha( objeto & );
-void cambiarprecio( objeto & );
-void cambiarvigen( objeto & );
+void cambios(objeto &);
+
+void showChangeOptions();
+
+int getChangeOption();
+
+bool isChangeOptionValid(std::string);
+
+void cambiarNombre(objeto &);
+
+void cambiarDescripcion(objeto &);
+
+void cambiarExistenciasMin(objeto &);
+
+void cambiarExistenciasMax(objeto &);
+
+void cambiarExistencias(objeto &);
+
+void cambiarFechaCaducidad(objeto &);
+
+void cambiarPrecio(objeto &);
 
 #endif // CAMBIOS_H
--- src/cambios.cpp
+++ src/cambios.cpp
@@ -1,4 +1,107 @@
-#include "./objeto.cpp"
+#include "../include/cambios.hpp"
+#include <iostream>
+#include <limits>
+#include <fstream>
+
+void cambios(objeto &b)
+{
+    int option;
+
+    showChangeOptions();
+    option = getChangeOption();
+    
+    switch(option)
+    {
+        case 1: 
+            cambiarnom(b);
+            break;
+        case 2: 
+            cambiardesc(b);
+            break;
+        case 3: 
+            cambiarexis(b);
+            break;
+        case 4: 
+            cambiarfecha(b);
+            break;
+        case 5: 
+            cambiarvigen(b);
+            break;
+        case 6: 
+            cambiarprecio(b);
+            break;
+        case 7:
+            std::cout << "Saliendo... Presione una tecla para continuar." << std::endl;
+            std::cin.get();
+            break;
+    }
+}
+
+void showChangeOptions()
+{
+    std::cout << "QUE DESEA CAMBIAR: \n"
+              <<"1 -> Nombre del Articulo\n"
+              <<"2 -> Descripcion generica\n"
+              <<"3 -> Stock\n"
+              <<"4 -> Fecha de Caducidad\n"
+              <<"5 -> vigencia\n"
+              <<"6 -> Precio\n"
+              <<"7 -> Cancelar\n";
+}
+
+int getChangeOption()
+{
+    std::string option = "";
+    while (true)
+    {
+        std::cout <<"Dame una opcion: ";
+        std::cin >> option;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (isChangeOptionValid(option))
+            return std::atoi(option.c_str());
+        else
+        {
+            std::cout << "Opcion invalida, intentelo de nuevo" << std::endl;
+            option.clear();
+        }
+    }
+}
+
+bool isChangeOptionValid(std::string input)
+{
+    if (input.length() != 1)
+        return false;
+
+    if (!isdigit(input[0]))
+        return false;
+    
+    int option = std::atoi(input.c_str());
+    if (option < 1 || option > 7)
+        return false;
+    
+    return true;
+}
+
+void cambiarNombre(objeto &b)
+{
+    std::fstream archive("base.dat", std::ios::binary|std::ios::in|std::ios::out);
+    // Get item
+    // Get change
+    // Write change
+    // Exit
+}
+
+void cambiarDescripcion(objeto &b){}
+
+void cambiarExistenciasMin(objeto &b){}
+
+void cambiarExistenciasMax(objeto &b){}
+
+void cambiarExistencias(objeto &b){}
+
+void cambiarFechaCaducidad(objeto &b){}
+
+void cambiarPrecio(objeto &b){}
 
 void cambiarnom( objeto &b ){
 /*
